65c36c78b3a7f6bd1fe1af6035c5eb3ebaf16ee5
Mark Pasricha <markp862@gmail.com>
2021-01-19 19:31:55 -0500

    added basic structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,10 @@ function renderDay(){
 			timeOfDay = i - 12;
 		}
 		var timeSpot = $("<div>").attr({"class": "col-md-1"}).text(`${timeOfDay} ${amOrPm}`);
-		var textArea = $("<textarea>").css({"min-width": "100%", "float": "left"});
-		var saveBtn = $("<button>").text("Save");
+		var textArea = $("<textarea>").attr({"class": "col-md-10"});
+		var saveBtn = $("<button>").attr({"class": "col-md-1"}).text("Save");
 
-		textArea.append(saveBtn);
-		timeSpot.append(textArea); 
-		timeRow.append(timeSpot);
+		timeRow.append(timeSpot, textArea, saveBtn);
 		calendar.append(timeRow)
 
 	}
