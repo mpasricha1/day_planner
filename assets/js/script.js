c129fc3449bf35ebc1ec4b5def4db51b261e614c
Mark Pasricha <markp862@gmail.com>
2021-01-19 20:02:13 -0500

    fixing ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,20 @@ function renderDay(){
 			timeOfDay = i - 12;
 		}
 		var timeSpot = $("<div>").attr({"class": "col-md-1"}).text(`${timeOfDay} ${amOrPm}`);
-		var textArea = $("<input>").attr({"class": "col-md-10", "type": "text"});
-		var saveBtn = $("<button>").attr({"class": "btn btn-info col-md-1"}).text("Save");
+		var textArea = $("<input>").attr({"class": "col-md-10", "type": "text", "id":`${i}text`});
+		var saveBtn = $("<button>").attr({"class": "btn btn-info col-md-1", "id": `${i}btn`}).text("Save");
 
 		timeRow.append(timeSpot, textArea, saveBtn);
 		calendar.append(timeRow)
-
 	}
 }
+$(document).ready(function() {
+
+	$("button").on("click", function(){
+		var buttonId = $(this).attr("id"); 
+		console.log(buttonId)
+		// alert(`Button number ${buttonId} clicked`)
+	});
+});
 
 renderDay();
