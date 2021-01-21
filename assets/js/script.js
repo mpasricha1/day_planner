e8ab8a88b61691387f8aeee3ea0b51208c076b0d
Mark Pasricha <markp862@gmail.com>
2021-01-20 20:31:12 -0500

    worked out color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ function renderDay(){
 
 		var inputGroup = $("<div>").attr({"class": "input-group mb-3", "id":`${i}`});
 		var timeSpot = $("<div>").attr({"class": "col-md-1"}).text(`${timeOfDay} ${amOrPm}`);
-		var textArea = $("<input>").attr({"class": "form-control col-md-10 ", "type": "text", "id":`input${i}`});
+		var textArea = $("<input>").attr({"class": "form-control col-md-10", "type": "text", "id":`t${i}`});
 		var inputGroup2 = $("<div>").attr({"class": "input-group-append"});
 		var submitBtn = $("<button>").attr({"class": "btn btn-info", "type": "button"}).text("Submit")
 
@@ -35,7 +35,7 @@ function renderDay(){
 
 function saveData(){
 	taskObject.timeSlot = $(this).parent().attr("id");
-	taskObject.task = $(`#input${taskObject.timeSlot}`).val();
+	taskObject.task = $(`#t${taskObject.timeSlot}`).val();
 
 	if(localStorage.getItem(`${currentDate}`) === null){
 	 	dailyTasks.push(taskObject);
@@ -50,13 +50,28 @@ function saveData(){
 function loadData(){
 	if(localStorage.getItem(`${currentDate}`) !== null){
 		var data = JSON.parse(localStorage.getItem(`${currentDate}`))
-		console.log(data)
 
 		data.forEach(datum => {
 			console.log(datum.timeSlot);
-			$(`#input${datum.timeSlot}`).val(datum.task);
+			$(`#t${datum.timeSlot}`).val(datum.task);
 		})
 	}
+}; 
+
+function colorCodeTasks(){
+	var hour = 13
+	// moment().format("HH"); 
+	$(".col-md-10").each(function(){
+		var taskHour = parseInt($(this).attr("id").substring(1));
+		console.log(taskHour);
+		if(taskHour < hour){
+			$(this).css({"background-color": "#778899", "color": "white"});
+		}else if(taskHour > hour){
+			$(this).css({"background-color": "#CD5C5C", "color": "white"});
+		}else{
+			$(this).css({"background-color": "#90EE90", "color": "white"});
+		}
+	})
 }
 $(document).ready(function() {
 
@@ -65,3 +80,4 @@ $(document).ready(function() {
 
 renderDay();
 loadData();
+colorCodeTasks();
